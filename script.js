8721e463c0f1a25e7a1903a3922173d0ce3f7a49
isaiyahO14 <isaiyahortega@gmail.com>
2025-08-08 17:27:12 +0000

    added our testimonials page

--- script.js
+++ script.js
@@ -1,4 +1,77 @@
-console.log("Hello World");
+const reviews = [
+     {
+        id: 1,
+        name: 'Susan Smith',
+        job: 'tech',
+        img: '#',
+        text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+     },
+     {
+        id: 2,
+        name: 'Beth mailen',
+        job: 'teached',
+        img: '#',
+        text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+     },
+     {
+        id: 3,
+        name: 'bella ortega',
+        job: 'none',
+        img: '#',
+        text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+     }
+];
+
+const img = document.getElementById('persin-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySeldctor('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentItem = 0;
+
+//load initial item
+window.addEventListener('DOMContentLoaded',function () {
+   const item = reviews[currentItem];
+   img.src = item.img
+   author.textContent = item.name
+   job.textContent = item.job
+   info.textContent = item.text
+});
+
+function showPerson(person){
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+
+nextBtn.addEventListener('click', function (){
+    currenrItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+prevBtn.addEventListener('click', function () {
+    currentItem--;
+    if (currentItem < 0){
+        currentItem = reviews.length - 1
+    }
+    showPerson(currentItem);
+});
+
+
+
+
+
+
+/* console.log("Hello World");
 /*
 //String has quotes/Number no quotes
 let myTest ="123";
@@ -23,7 +96,7 @@ function introduction(name,age){
 
 introduction("isaiyah", 14)*/ 
 
-const hour = new Date().getHours();
+/* const hour = new Date().getHours();
 let greeting = "";
 
 if (hour<12){
